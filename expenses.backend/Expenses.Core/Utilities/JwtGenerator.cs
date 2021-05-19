e75d7349c13635d134bf9e6c8728ce6ff183166f
David Asenov <david.asenoff@gmail.com>
2021-05-19 22:21:28 +0300

    Update JwtGenerator.cs

--- expenses.backend/Expenses.Core/Utilities/JwtGenerator.cs
+++ expenses.backend/Expenses.Core/Utilities/JwtGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -21,6 +22,22 @@ namespace Expenses.Core.Utilities
         private static string GenerateUserToken(Claim[] claims, DateTime expires)
         {
             var tokenHadler = new JwtSecurityTokenHandler();
+            // The code bellow depends on adding some keys in launchSettings.json as follows:
+            // "JWT_SECRET": "Some super secret string is needed here",
+            // "JWT_ISSUER": "My Expenses"
+            var secret = Environment.GetEnvironmentVariable("JWT_SECRET");
+            var issuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
+            var key = Encoding.ASCII.GetBytes(secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = expires,
+                Issuer = issuer,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHadler.CreateToken(tokenDescriptor);
+
+            return tokenHadler.WriteToken(token);
         }
     }
 }
