922f2580c595c70cb631af3227a78c5c3d3bf609
David Asenov <david.asenoff@gmail.com>
2021-05-19 21:59:41 +0300

    Add AuthenticationController

---
+++ expenses.backend/expenses.webapi/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using Expenses.Core;
+using Expenses.Core.CustomException;
+using Expenses.DB;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expenses.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public AuthenticationController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp(User user)
+        {
+            try
+            {
+                var result = await userService.SignUp(user);
+                return Created("", result);
+            }
+            catch (UserNameAlreadyExistException e)
+            {
+                return StatusCode(409, e.Message);
+            }
+        }
+    }
+}
