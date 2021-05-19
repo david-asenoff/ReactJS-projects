356184db8a61746a8a657642754494e94d8989d6
David Asenov <david.asenoff@gmail.com>
2021-05-19 21:58:19 +0300

    Create InvalidUserNamePasswordException.cs

---
+++ expenses.backend/Expenses.Core/CustomException/InvalidUserNamePasswordException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Expenses.Core.CustomException
+{
+    public class InvalidUserNamePasswordException : Exception
+    {
+        public InvalidUserNamePasswordException()
+        {
+        }
+
+        public InvalidUserNamePasswordException(string message) : base(message)
+        {
+        }
+
+        public InvalidUserNamePasswordException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidUserNamePasswordException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
