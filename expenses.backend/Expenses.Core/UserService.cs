0f6fa3de3f25102a7487f84d6d7d9da8b180463f
David Asenov <david.asenoff@gmail.com>
2021-05-19 21:58:50 +0300

    Create UserService

---
+++ expenses.backend/Expenses.Core/UserService.cs
@@ -0,0 +1,57 @@
+﻿using Expenses.Core.CustomException;
+using Expenses.Core.DTO;
+using Expenses.DB;
+using Microsoft.AspNet.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expenses.Core
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext context;
+        private readonly IPasswordHasher passwordHasher;
+
+        public UserService(AppDbContext context,
+                           IPasswordHasher passwordHasher)
+        {
+            this.context = context;
+            this.passwordHasher = passwordHasher;
+        }
+
+        public async Task<AuthenticatedUser> SignIn(User user)
+        {
+            var userDb = await context.Users.FirstOrDefaultAsync(x => x.Username == user.Username);
+            if (userDb == null ||
+                passwordHasher.VerifyHashedPassword(userDb.Password, user.Password) == PasswordVerificationResult.Failed)
+            {
+                throw new InvalidUserNamePasswordException("Invalid username or password");
+            }
+            return new AuthenticatedUser
+            {
+                Username = user.Username,
+                Token = "test token"
+            }
+        }
+
+        public async Task<AuthenticatedUser> SignUp(User user)
+        {
+            var checkUser = await context.Users.FirstOrDefaultAsync(x => x.Username.Equals(user.Username));
+            if (checkUser != null)
+            {
+                throw new UserNameAlreadyExistException("Username already exists");
+            }
+            user.Password = passwordHasher.HashPassword(user.Password);
+            await context.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            return new AuthenticatedUser
+            {
+                Username = user.Username,
+                Token = "test token",
+            };
+        }
+    }
+}
